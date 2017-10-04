a689d5ce84a5574e9cc5fec1978a787b555becb8
Henry Vu <henryvu25@gmail.com>
2017-10-04 13:45:46 -0700

    added attacks dictionary

--- BaseCard.cs
+++ BaseCard.cs
@@ -20,5 +20,40 @@ namespace Pokemon
 			this.SpDef = SpDef;
 			this.Speed = Speed;
 		}
+        public int CalculateAttack(BaseCard defender, BaseAttack attack)
+        {
+            return 0;
+        }
+       
 	}
+    public class BaseAttack{
+            public string Name {get; set;}
+            public string Type {get; set;}
+            public string Category {get; set;}
+            public string Contest {get; set;}
+            public int PP {get; set;}
+            public int Power {get; set;}
+            public int Accuracy {get; set;}
+            public string Gen {get; set;}
+
+public BaseAttack(string Name, string Type, string Category, string Contest, string PP, string Power, string Accuracy, string Gen)
+{
+    int number;
+    this.Name = Name;
+    this.Type = Type;
+    this.Category = Category;
+    this.Contest = Contest;
+    this.PP = 0;
+    if (Int32.TryParse(PP, out number))
+        this.PP = number; 
+    this.Power = 0;
+    if (Int32.TryParse(Power, out number))
+        this.Power = number; 
+    this.Accuracy = 0;
+    if (Int32.TryParse(Accuracy, out number))
+        this.Accuracy = number; 
+    this.Gen = Gen;
+}
+    
+        }
 }
